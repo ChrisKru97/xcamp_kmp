e148cc4c961fb3d85693197e6ab17c932def59bf
Christian Krutsche <christian.krutsche@gmail.com>
2026-02-08 20:31:35 +0100

    refactor: Update color-space to display-p3 for improved color accuracy across assets; enhance SplashView layout for better responsiveness

--- iosApp/iosApp/components/SplashView.swift
+++ iosApp/iosApp/components/SplashView.swift
@@ -2,15 +2,15 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        ZStack {
-            Color.background
-                .ignoresSafeArea()
-
+        Group {
             Image("logo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(maxWidth: 300)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
+        .background(Color.background)
     }
 }
 
